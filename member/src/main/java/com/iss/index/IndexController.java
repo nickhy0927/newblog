d617b636c5713e8dc2fcdde04d5fdb99c2289795
yuanhuangd <h_y_12@163.com>
2017-09-28 10:33:33 +0800

    修改依赖

--- member/src/main/java/com/iss/index/IndexController.java
+++ member/src/main/java/com/iss/index/IndexController.java
@@ -1,5 +1,13 @@
 package com.iss.index;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class IndexController {
+
+    public String index() {
+
+        return "index";
+    }
 
 }
